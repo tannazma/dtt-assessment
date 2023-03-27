8d7932ea0400de517d3ca8b3a86cc851959841c4
tannazma <97254921+tannazma@users.noreply.github.com>
2023-03-27 21:36:31 +0200

    create Header compopnent + adding logo in header

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <img width="100" src="src/assets/img_logo_dtt@3x.png" />
+    <RouterLink to="/list">Houses</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </div>
+</template>
