912d1369e666d796c4429d3df14e080b2e0e6d09
tannazma <97254921+tannazma@users.noreply.github.com>
2023-03-27 21:52:13 +0200

    create the second part of list page + format

--- src/views/ListOfHouses.vue
+++ src/views/ListOfHouses.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
-import Header from '@/components/Header.vue';
+import Header from '@/components/Header.vue'
 import { houses } from '@/stores/houses'
 </script>
 
 <template>
-    <div>
-        <Header/>
-    </div>
   <div>
-    <div v-for="House in houses" :key="House.id" style="border: 1px solid black; margin: 20px; padding: 10px;">
-      <strong>ID</strong>: {{ House.id }}
-      <br />
-      <strong>Address</strong>: {{ House.name }}
-      <br />
-      {{ House.descrption }}
-    </div>
+    <Header />
+    <h1>Houses</h1>
+    <button>+ CREATE NEW</button>
+  </div>
+  <div>
+    <input placeholder="Search for a house" />
+    <button>Price</button>
+    <button>Size</button>
+  </div>
+  <div
+    v-for="House in houses"
+    :key="House.id"
+    style="border: 1px solid black; margin: 20px; padding: 10px"
+  >
+    <strong>ID</strong>: {{ House.id }}
+    <br />
+    <strong>Address</strong>: {{ House.name }}
+    <br />
+    {{ House.descrption }}
   </div>
 </template>
