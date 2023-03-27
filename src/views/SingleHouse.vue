3ac6ce83d8c7382e6bcd10334597b85f1c6f0026
tannazma <97254921+tannazma@users.noreply.github.com>
2023-03-27 14:08:54 +0200

    show the route.params.id and the name of house in the single house page

--- src/views/SingleHouse.vue
+++ src/views/SingleHouse.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { houses } from '@/stores/houses'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
@@ -7,5 +8,8 @@ console.log(route.params.id)
 </script>
 
 <template>
-  <div>This is the house you were looking for house {{ route.params.id }}.</div>
+  <div>
+    This is the house you were looking for house {{ route.params.id }}
+    {{ houses.find(house => house.id== Number(route.params.id)) ?.name }} 
+  </div>
 </template>
