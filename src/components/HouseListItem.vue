348d81c4ae657073a7c8ea609f5f6890272c80a2
tannazma <97254921+tannazma@users.noreply.github.com>
2023-03-28 11:53:10 +0200

    add image and price and address changed in list houses page

--- src/components/HouseListItem.vue
+++ src/components/HouseListItem.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
+
 defineProps<{
   house: {
     id: number
     name: string
-    descrption: string
+    address: string
+    price: string
   }
 }>()
 </script>
 
 <template>
   <div style="border: 1px solid black; margin: 20px; padding: 10px">
+    <img
+      display="flex,"
+      flex="1,"
+      align-content="space-between,"
+      width="100"
+      src="src/assets/img_placeholder_house@3x.png"
+    />   
+    <br /> 
     <strong>ID</strong>: {{ house.id }}
     <br />
-    <strong>Address</strong>: {{ house.name }}
+    <strong>Name</strong>: {{ house.name }}
     <br />
-    {{ house.descrption }}
+    <string>Price</string>: {{ house.price }}
+    <br />
+    {{ house.address }}
   </div>
 </template>
--- src/stores/houses.ts
+++ src/stores/houses.ts
@@ -2,16 +2,19 @@ export const houses = [
   {
     id: 5,
     name: 'Newa 37',
-    descrption: 'This is in The Netherlands'
+    price:  '€ 500 000',
+    address: 'This is in The Netherlands'
   },
   {
     id: 6,
     name: 'Pazouki 20',
-    descrption: 'This is in Tehran Iran'
+    price: '€ 500 000',
+    address: 'This is in Tehran Iran'
   },
   {
     id: 7,
     name: 'Tigranyan 34',
-    descrption: 'This is in Armenia'
+    price:'€ 500 000',
+    address: 'This is in Armenia'
   }
 ]
