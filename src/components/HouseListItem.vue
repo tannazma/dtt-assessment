96a6f1d4d984c5f0cf753178272316e5569386a0
tannazma <97254921+tannazma@users.noreply.github.com>
2023-03-28 20:16:53 +0200

    add dynamic urls for single houses using router

--- src/components/HouseListItem.vue
+++ src/components/HouseListItem.vue
@@ -17,15 +17,17 @@ defineProps<{
 </script>
 
 <template>
-  <div style="border: 1px solid black; margin: 20px; padding: 10px">
-    <img width="100" :src="house.image" />
-    <br />
-    <strong>ID</strong>: {{ house.id }}
-    <br />
-    <img width="30" src="src/assets/ic_bath@3x.png" />
-    <p>bedrooms:{{ house.rooms.bedrooms }}</p>
-    <string>Price</string>: {{ house.price }}
-    <br />
-    {{ house.description }}
-  </div>
+  <RouterLink :to="'/house/' + house.id">
+    <div style="border: 1px solid black; margin: 20px; padding: 10px">
+      <img width="100" :src="house.image" />
+      <br />
+      <strong>ID</strong>: {{ house.id }}
+      <br />
+      <img width="30" src="src/assets/ic_bath@3x.png" />
+      <p>bedrooms:{{ house.rooms.bedrooms }}</p>
+      <string>Price</string>: {{ house.price }}
+      <br />
+      {{ house.description }}
+    </div>
+  </RouterLink>
 </template>
