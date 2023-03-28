076d65c5778fb6ec336d483ce791191c20a99e11
tannazma <97254921+tannazma@users.noreply.github.com>
2023-03-28 20:22:21 +0200

    fix the heaader logo link and remove unnecessory stuf in header and add header in single house page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,11 +5,8 @@ import { RouterLink } from 'vue-router'
 <template>
   <div class="header-container">
     <img
-      display="flex,"
-      flex="1,"
-      align-content="space-between,"
       width="100"
-      src="src/assets/img_logo_dtt@3x.png"
+      src="/src/assets/img_logo_dtt@3x.png"
     />
     <RouterLink class="list" to="/list">Houses</RouterLink>
     <RouterLink class="about" to="/about">About</RouterLink>
--- src/views/SingleHouse.vue
+++ src/views/SingleHouse.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { houses } from '@/stores/houses'
 import { useRoute } from 'vue-router'
+import Header from '@/components/Header.vue';
 
 const route = useRoute()
 
@@ -8,8 +9,9 @@ console.log(route.params.id)
 </script>
 
 <template>
+  <Header />
   <div>
     This is the house you were looking for house {{ route.params.id }}
-    {{ houses.find(house => house.id== Number(route.params.id)) ?.name }} 
+    {{ houses.find(house => house.id== Number(route.params.id)) ?.description }} 
   </div>
 </template>
