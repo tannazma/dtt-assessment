9ba3e031f4d2f3686a3bebbc59a93baac18af196
tannazma <97254921+tannazma@users.noreply.github.com>
2023-04-07 16:35:35 +0200

    add styles to the house component items + adding icons for bedroom, bathroom and size

--- src/components/HouseListItem.vue
+++ src/components/HouseListItem.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { T_House } from '@/types/house';
+import type { T_House } from '@/types/house'
 
 defineProps<{
   house: T_House
@@ -8,16 +8,33 @@ defineProps<{
 
 <template>
   <RouterLink :to="'/house/' + house.id">
-    <div style="border: 1px solid black; margin: 20px; padding: 10px">
-      <img width="100" :src="house.image" />
-      <br />
-      <strong>ID</strong>: {{ house.id }}
-      <br />
-      <img width="30" src="src/assets/ic_bath@3x.png" />
-      <p>bedrooms:{{ house.rooms.bedrooms }}</p>
-      <strong>Price</strong>: {{ house.price }}
-      <br />
-      {{ house.description }}
+    <div style="background-color:white; border-radius:3%;margin: auto; gap: 30px;">
+    <div style="margin-bottom: 10px; padding: 10px; display: flex">
+      <img width="130" height="130" :src="house.image" style="margin-right: 20px; border-radius: 5%;" />
+      <div style="display: flex; flex-direction: column">
+        <strong style="color:black">{{ house.location.street }}</strong>
+        <strong style="color:black">€ {{ house.price }}</strong>
+        <p style="margin-top: 0px; color: gray">
+          {{ house.location.zip }} {{ house.location.city }}
+        </p>
+        <div style="display: flex; margin-top: 20px">
+          <p style="margin-right: 20px; display: flex; align-items: center; color: gray;">
+            <img width="20" src="src/assets/ic_bed@3x.png" style="margin-right: 8px" />
+            {{ house.rooms.bedrooms }}
+          </p>
+          <p style="margin-right: 20px; display: flex; align-items: center; color: gray;">
+            <img width="20" src="src/assets/ic_bath@3x.png" style="margin-right: 8px" />{{
+              house.rooms.bathrooms
+            }}
+          </p>
+          <p style="display: flex; align-items: center; color: gray">
+            <img width="20" src="src/assets/ic_size@3x.png" style="margin-right: 8px" />
+            {{ house.size }} m2
+          </p>
+        </div>
+      </div>
+      <!-- {{ house.description }} -->
     </div>
+  </div>
   </RouterLink>
 </template>
