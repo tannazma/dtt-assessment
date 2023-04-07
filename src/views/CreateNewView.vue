21439f4873ddb2cc6614f52f2393b7111bab8a47
tannazma <97254921+tannazma@users.noreply.github.com>
2023-04-07 17:31:51 +0200

    add background image for the form page

--- src/views/CreateNewView.vue
+++ src/views/CreateNewView.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import Header from '@/components/Header.vue';
+import Header from '@/components/Header.vue'
 import NewHouseForm from '@/components/NewHouseForm.vue'
 </script>
 
 <template>
+  <div style="background-image: url(src/assets/img_background@3x.png); background-size: 100%;">
     <Header />
-  <div>
-    <p>
-      Back to overview
-    </p>
-  </div>
-  <div class="create-new">
-    <h1>Create new listing</h1>
-    <NewHouseForm />
+    <div>
+      <p>Back to overview</p>
+    </div>
+    <div class="create-new">
+      <h1>Create new listing</h1>
+      <NewHouseForm />
+    </div>
+    
   </div>
 </template>
