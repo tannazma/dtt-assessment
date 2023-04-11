83cf353b6e474dab05413e3f99de494aeda8f812
tannazma <97254921+tannazma@users.noreply.github.com>
2023-04-11 16:49:48 +0200

    create single house page + style

--- src/views/SingleHouse.vue
+++ src/views/SingleHouse.vue
@@ -24,16 +24,60 @@ getHouseFromServer()
 </script>
 
 <template>
-  <Header />
   <div>
-    This is the house you were looking for house {{ route.params.id }}
-    <img
-      width="130"
-      height="130"
-      :src="state.house?.image"
-      style="margin-right: 20px; border-radius: 5%"
-    />
-    {{ state.house?.description }}
-    {{ state.house?.price }}
+    <Header />
+    <div>
+      <p style="margin-top: 20px">Back to overview</p>
+    </div>
+    <div style="background-color: white; margin-top: 20px">
+      <div>
+        <img class="image" :src="state.house?.image" style="margin-right: 20px; margin-top: 0px" />
+      </div>
+      <div style="padding: 20px; margin: 20px">
+        <div style="margin-bottom: 20px">
+          <h1 style="margin-bottom: 20px">
+            {{ state.house?.location.street }}
+          </h1>
+          <p style="margin-bottom: 10px">
+            <img width="15" src="/src/assets/ic_location@3x.png" />
+            {{ state.house?.location.zip }} {{ state.house?.location.city }}
+          </p>
+          <div style="display: flex; gap: 15px">
+            <p style="margin-bottom: 10px">
+              <img width="15" src="/src/assets/ic_price@3x.png" />
+              {{ state.house?.price }}
+            </p>
+            <p><img width="15" src="/src/assets/ic_size@3x.png" /> {{ state.house?.size }} m2</p>
+            <p>
+              <img width="15" src="/src/assets/ic_construction_date@3x.png" /> Built in
+              {{ state.house?.constructionYear }}
+            </p>
+          </div>
+          <div style="display: flex; gap: 15px">
+            <p>
+              <img width="15" src="/src/assets/ic_bed@3x.png" />
+              {{ state.house?.rooms.bedrooms }}
+            </p>
+            <p>
+              <img width="15" src="/src/assets/ic_bath@3x.png" />
+              {{ state.house?.rooms.bathrooms }}
+            </p>
+            <p>
+              <img width="15" src="/src/assets/ic_garage@3x.png" />
+              {{ state.house?.hasGarage }}
+            </p>
+          </div>
+          <p style="margin-top: 20px">
+            {{ state.house?.description }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+.image {
+  width: 100%;
+}
+</style>
