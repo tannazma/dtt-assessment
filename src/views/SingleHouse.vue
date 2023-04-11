ec3edb97bb40c9545cb0c6943aadbae5d4dbe3bc
tannazma <97254921+tannazma@users.noreply.github.com>
2023-04-11 14:42:24 +0200

    get a house from houses api, and find the house.id to shiw the state of the house in the single house page

--- src/views/SingleHouse.vue
+++ src/views/SingleHouse.vue
@@ -1,17 +1,39 @@
 <script setup lang="ts">
-import { houses } from '@/stores/houses'
 import { useRoute } from 'vue-router'
 import Header from '@/components/Header.vue'
+import { reactive } from 'vue'
+import type { T_House } from '@/types/house'
 
 const route = useRoute()
-const house = houses.find((house) => house.id === Number(route.params.id))
+
+const state = reactive<{
+  house: T_House | undefined
+}>({ house: undefined })
+
+async function getHouseFromServer() {
+  const response = fetch('https://api.intern.d-tt.nl/api/houses', {
+    headers: {
+      'X-Api-Key': 'ndFAUDTBMW7xO6YsIL3-Gb5rSQu4ZoHz'
+    }
+  })
+
+  const houses: T_House[] = await (await response).json()
+  state.house = houses.find((house) => house.id === Number(route.params.id))
+}
+getHouseFromServer()
 </script>
 
 <template>
   <Header />
   <div>
     This is the house you were looking for house {{ route.params.id }}
-    {{ house?.description }}
-    {{ house?.price }}
+    <img
+      width="130"
+      height="130"
+      :src="state.house?.image"
+      style="margin-right: 20px; border-radius: 5%"
+    />
+    {{ state.house?.description }}
+    {{ state.house?.price }}
   </div>
 </template>
