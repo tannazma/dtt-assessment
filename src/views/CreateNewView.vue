c9b994c5e5ee11a7f682b5933e0fe9ae58720cf8
tannazma <97254921+tannazma@users.noreply.github.com>
2023-04-17 13:49:32 +0200

    remove manual header in the create house page

--- src/views/CreateNewView.vue
+++ src/views/CreateNewView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import Header from '@/components/Header.vue'
 import NewHouseForm from '@/components/NewHouseForm.vue'
 </script>
 
 <template>
   <div style="background-image: url(src/assets/img_background@3x.png); background-size: 100%;">
-    <Header />
     <div>
       <p>Back to overview</p>
     </div>
