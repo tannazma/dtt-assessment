09d3d30bc9df4e538ddc426f92f6a64ec1bb8b4f
tannazma <97254921+tannazma@users.noreply.github.com>
2023-04-18 13:55:33 +0200

    add router link for back to overview + add house and isEditing props in newhouseform component + add state here

--- src/views/CreateNewView.vue
+++ src/views/CreateNewView.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import NewHouseForm from '@/components/NewHouseForm.vue'
+import type { T_House } from '@/types/house'
+import { reactive } from 'vue'
+
+const state = reactive<{
+  house?: T_House
+}>({ house: undefined })
 </script>
 
 <template>
-  <div style="background-image: url(src/assets/img_background@3x.png); background-size: 100%;">
-    <div>
-      <p>Back to overview</p>
+  <div style="background-image: url(src/assets/img_background@3x.png); background-size: 100%">
+    <div style="padding-top: 30px; padding-bottom: 20px">
+      <RouterLink :to="'/list/'">
+        <p style="color: black">
+          <img src="src/assets/ic_back_grey@3x.png" width="15" style="text-align: center" />
+          Back to overview
+        </p>
+      </RouterLink>
     </div>
     <div class="create-new">
       <h1>Create new listing</h1>
-      <NewHouseForm />
+      <NewHouseForm :isEditing="false" :house="state.house" />
     </div>
-    
   </div>
 </template>
