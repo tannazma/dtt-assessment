a27d4421f070339868321df2a57389d07d2ffc28
tannazma <97254921+tannazma@users.noreply.github.com>
2023-05-09 14:34:10 +0200

    automatically redirect houses page for default page by using router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      redirect: '/list'
+    },
     {
       path: '/about',
       name: 'about',
